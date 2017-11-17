3cf87b754edb753e3334c7ce14fd4c13bf539508
Arun Gupta <arun.gupta@gmail.com>
2017-11-16 16:36:28 -0800

    updating instructions - still needs further work

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -5,6 +5,7 @@ package org.examples.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("GREETING") + System.getProperty("NAME"));
+        System.out.println("prop: " + System.getProperty("GREETING") + System.getProperty("NAME"));
+        System.out.println("env: " + System.getenv("GREETING") + System.getenv("NAME"));
     }
 }
