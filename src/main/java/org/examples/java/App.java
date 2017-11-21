fd45c409745451b2c4cd6caabb9c143f006e93e2
Arun Gupta <arun.gupta@gmail.com>
2017-11-21 11:23:45 -0800

    using REST API

--- src/main/java/org/examples/java/App.java
+++ src/main/java/org/examples/java/App.java
@@ -4,6 +4,8 @@ import com.bettercloud.vault.SslConfig;
 import com.bettercloud.vault.Vault;
 import com.bettercloud.vault.VaultConfig;
 import com.bettercloud.vault.VaultException;
+import okhttp3.*;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -21,6 +23,9 @@ import static javax.swing.UIManager.put;
  * Hello world!
  */
 public class App {
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
     public static void main(String[] args) throws VaultException, IOException {
 //        System.out.println("prop: "
 //                + System.getProperty("GREETING")
@@ -28,19 +33,39 @@ public class App {
         System.out.println("Connecting to Vault: " + System.getenv("VAULT_ADDR"));
         final VaultConfig config = new VaultConfig()
                 .address(System.getenv("VAULT_ADDR"))
+//                .token(System.getenv("TOKEN"))
                 .sslConfig(new SslConfig().verify(false).build())
                 .build();
         final Vault vault = new Vault(config);
         String serviceToken = new String(Files.readAllBytes(Paths.get("/var/run/secrets/kubernetes.io/serviceaccount/token")));
         System.out.println("serviceToken: " + serviceToken);
-        Map<String, String> map = vault.logical().write("/auth/kubernetes/login",
-                new HashMap<String, Object>() {
-                    {
-                        put("role", "demo");
-                        put("jwt", serviceToken);
-                    }
-                }).getData();
-        System.out.println("vaultToken: " + map);
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+        RequestBody body = RequestBody.create(JSON, "{\n" +
+                "    \"role\": \"vault-reviewer\",\n" +
+                "    \"jwt\": \"" + serviceToken + "\"\n" +
+                "}");
+        Request request = new Request.Builder()
+                .url(System.getenv("VAULT_ADDR") + "/v1/auth/kubernetes/login")
+                .post(body)
+                .build();
+        try (Response response = httpClient.newCall(request).execute()) {
+            System.out.println("vaultToken: " + response.body().string());
+        }
+
+//        Map<String, String> map = vault.logical().write("/auth/kubernetes/login",
+//                new HashMap<String, Object>() {
+//                    {
+//                        put("role", "vault-auth");
+//                        put("jwt", serviceToken);
+//                    }
+//                }).getData();
+//        System.out.println("vaultToken: " + map);
 
         System.out.println("vault: "
                 + vault
